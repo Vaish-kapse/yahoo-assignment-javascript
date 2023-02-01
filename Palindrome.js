23cff2a99603913b0a2fa5469546f7cbddaaf1a1
Vaishali kapse <121084240+Vaish-kapse@users.noreply.github.com>
2023-02-01 11:24:42 +0530

    Update Palindrome.js

--- Palindrome.js
+++ Palindrome.js
@@ -2,10 +2,9 @@ function isPalindrome(string) {
   return string === string.split('').reverse().join('');
 }
 console.log(isPalindrome("racecar"))
-// true
+
 console.log(isPalindrome("madam"))
-// true
+                         
 console.log(isPalindrome("kavitha"))
-// false
+
 console.log(isPalindrome("keyboard"))
-// false
